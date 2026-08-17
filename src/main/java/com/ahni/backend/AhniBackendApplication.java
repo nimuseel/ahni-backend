744b674f8f6f8e509870f03e8b0181fb9f28c3d3
nimusee1 <l08c0739@gmail.com>
2026-08-17 17:39:56 +0900

    chore: initialize ahni backend

---
+++ src/main/java/com/ahni/backend/AhniBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ahni.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhniBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhniBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahni/backend/AhniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahni.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhniBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

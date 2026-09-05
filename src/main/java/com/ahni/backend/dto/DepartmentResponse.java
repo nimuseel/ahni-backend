d339a9d79fe28e45c6c13fc63d9b8dca8436f7ae
nimusee1 <l08c0739@gmail.com>
2026-09-05 16:32:28 +0900

    feat: openapi schema 맞추기

--- src/main/java/com/ahni/backend/dto/DepartmentResponse.java
+++ src/main/java/com/ahni/backend/dto/DepartmentResponse.java
@@ -1,9 +1,19 @@
 package com.ahni.backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
 public record DepartmentResponse(
+    @Schema(
+        description = "학과 식별자",
+        example = "00000000-0000-0000-0000-000000000001"
+    )
     UUID entityId,
+    @Schema(
+        description = "학과명",
+        example = "소프트웨어융합공학과"
+    )
     String name
 ) {
 }
